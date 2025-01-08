46aa00c88e8fbe7bdca1b19ad4bd460087e44239
KTetwejer <115549398+KTetwejer@users.noreply.github.com>
2025-01-08 13:38:27 +0100

    main

---
+++ mixology_store/php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "pl">
+
+<head>
+	<title>MiksoShop</title>
+	<meta name="Author" content="Konrad Tetwejer" />
+	<link rel="stylesheet" href="../css/style.css"/>
+</head>
+
+<body>
+	
+</body>
+
+</html>
